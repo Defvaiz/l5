b4bbdd8e9ab4959a1a5b74eedec3c211d5bff421
Defvaiz <defvaiz3@gmail.com>
2023-05-04 20:03:23 +0300

    2

---
+++ Laba 5.0/Laba 5.0/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+
+
+class Regulars
+{
+    static void Main(string[] args)
+    {
+        string[] input = File.ReadAllLines(@"text.txt");
+        Dictionary<Regex, int> regexDict = new Dictionary<Regex, int> {
+            { new Regex(@"^a$"), 1 }, // a
+            { new Regex(@"^a{6}$"), 2 }, // aaaaaa
+            { new Regex(@"^a aa a$"), 3 }, // a aa a
+            { new Regex(@"^[A-Za-z0-9]{5,}[^aaaaaa]$"), 4 }, // Не менее 5-ти алфавитно-цифровых символов
+            { new Regex(@"^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+$"), 5 }, // email
+            { new Regex(@"^(?:[уУ][лЛ]\.\s+)?([а-яА-ЯёЁ]+(\-[а-яА-ЯёЁ]+)?)\,?\s+(?:[дД]\.\s+)?(\d+(?:[\-\/]\d+)?)$"), 6 } // Ул.строка д.число/число
+        };
+
+        foreach (var kvp in regexDict)
+        {
+            var regex = kvp.Key;
+            var number = kvp.Value;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (regex.IsMatch(input[i]))
+                {
+                    Console.WriteLine($"{number}) {input[i]}");
+                }
+            }
+        }
+
+        // добавляем ручной ввод адреса
+        Console.WriteLine("Введите адрес:");
+        string address = Console.ReadLine();
+
+        // создаем экземпляр Regex для адреса
+        Regex addressRegex = new Regex(@"^(?:[уУ][лЛ]\.\s+)?([а-яА-ЯёЁ]+(\-[а-яА-ЯёЁ]+)?)\,?\s+(?:[дД]\.\s+)?(\d+(?:[\-\/]\d+)?)$");
+
+        // применяем регулярное выражение к введенной строке
+        Match match = addressRegex.Match(address);
+        if (match.Success)
+        {
+            // получаем отдельно название улицы и номер дома
+            string streetName = match.Groups[1].Value;
+            string houseNumber = match.Groups[3].Value;
+            Console.WriteLine($"Название улицы: {streetName}");
+            Console.WriteLine($"Номер дома: {houseNumber}");
+        }
+        else
+        {
+            Console.WriteLine("Адрес не соответствует шаблону.");
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ Laba 5.0/Лаба 5 доп/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+string text = "Добро пожаловать в наш магазин, вот наши цены: 1 кг. яблоки - 90 руб., 2 кг. апельсины - 130 руб. Также в ассортименте орехи в следующей фасовке: 0.5 кг. миндаль - 500 руб";
+Regex rd1 = new Regex(@"((?:\d+)?\.?\d+)?\sкг\.\s(\w+)\s\-\s(\d+)\sруб\.?"); 
+var RawProducts = rd1.Matches(text);
+foreach (Match RawProduct in RawProducts)
+{
+    foreach (Match ProductString in rd1.Matches(RawProduct.Value))
+    {
+        var SplitProduct = ProductString.Groups;
+        Console.WriteLine($"{SplitProduct[2]} - {Convert.ToDouble(SplitProduct[3].Value) / Convert.ToDouble(SplitProduct[1].Value.Replace('.', ','))} руб/кг");
+    }
+
+}
